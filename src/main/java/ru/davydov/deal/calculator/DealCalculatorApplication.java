3cdc9e8933f876056e0edeb7134526c985aa3674
noion <n3t1m3n@gmail.com>
2021-07-07 18:25:14 +0300

    move check to test

--- src/main/java/ru/davydov/deal/calculator/DealCalculatorApplication.java
+++ src/main/java/ru/davydov/deal/calculator/DealCalculatorApplication.java
@@ -1,13 +1,9 @@
 package ru.davydov.deal.calculator;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import ru.davydov.deal.calculator.controller.Controller;
 import ru.davydov.deal.calculator.service.rest.tinkoff.FeignMarker;
 
 @Slf4j
@@ -17,13 +13,5 @@ public class DealCalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DealCalculatorApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        var controller = ctx.getBean(Controller.class);
-        controller.extractAllDeals();
-        return args -> {
-        };
     }
 }
--- src/test/java/ru/davydov/deal/calculator/DealCalculatorApplicationTests.java
+++ src/test/java/ru/davydov/deal/calculator/DealCalculatorApplicationTests.java
@@ -1,13 +1,21 @@
 package ru.davydov.deal.calculator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.davydov.deal.calculator.controller.Controller;
 
+@Slf4j
 @SpringBootTest
 class DealCalculatorApplicationTests {
 
+    @Autowired
+    private Controller controller;
+
     @Test
     void contextLoads() {
+        controller.extractAllDeals();
     }
 
 }
